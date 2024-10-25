3ce1cc3cdd4967c8b0cf8b6850b277dd1a6c3265
MAXII\maxii <maximaxmaxii690@gmail.com>
2024-10-25 19:00:32 -0300

    add application.yml

---
+++ src/main/java/com/maxi/pantrypos/PantryPosApplication.java
@@ -0,0 +1,13 @@
+package com.maxi.pantrypos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantryPosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PantryPosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maxi/pantrypos/PantryPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxi.pantrypos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantryPosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
